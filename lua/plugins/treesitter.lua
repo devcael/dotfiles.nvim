03fd25b83f3794fc87c9a7633230b211a92be968
devcael <caelgoncalves@icloud.com>
2024-07-18 15:03:39 -0300

    feat: altera o caminho para as libs do lsp javascript, e adiciona lsp para o hmtl

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,9 +2,6 @@ require 'nvim-treesitter.configs'.setup {
   ensure_installed = { "c" },
   sync_install = false,
   auto_install = true,
-  autotag = {
-    enable = true
-  },
   highlight = {
     enable = true,
   },
