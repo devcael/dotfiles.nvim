d0d8a195f37ea42302171db93af687ba02033f81
devcael <caelgoncalves@icloud.com>
2025-01-22 06:10:54 -0300

    feat: adiciona flutter tools e configurações no tema do rose-pine

--- lua/plugins/minimal_theme.lua
+++ lua/plugins/minimal_theme.lua
@@ -4,8 +4,8 @@ return {
     priority = 1000,
     'Yazeed1s/minimal.nvim',
     config = function()
-      vim.cmd("colorscheme minimal")
-      require("resources.theme_utils").set_background_transparent()
+      -- vim.cmd("colorscheme minimal")
+      -- require("resources.theme_utils").set_background_transparent()
     end
   }
 }
